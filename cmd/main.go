106714dee7a35863a2613396eb0000cac916d3f0
mpaulson <the.primeagen@gmail.com>
2023-11-05 19:46:20 -0700

    feat: part 3 and 4.  my fault. i ... didn't commit 3 by itself :)

--- cmd/main.go
+++ cmd/main.go
@@ -3,45 +3,163 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Template struct {
-    tmpl *template.Template
+	tmpl *template.Template
 }
 
 func newTemplate() *Template {
-    return &Template{
-        tmpl: template.Must(template.ParseGlob("views/*.html")),
-    }
+	return &Template{
+		tmpl: template.Must(template.ParseGlob("views/*.html")),
+	}
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-    return t.tmpl.ExecuteTemplate(w, name, data)
+	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
-type Count struct {
-    Count int
+type Contact struct {
+	Name  string
+	Email string
+	Id    int
+}
+
+type Data struct {
+	Contacts []Contact
+}
+
+func NewData() *Data {
+	return &Data{
+		Contacts: []Contact{
+			{
+				Name:  "John Doe",
+				Email: "john.doe@gmail.com",
+				Id:    1,
+			},
+			{
+				Name:  "Jane Doe",
+				Email: "jain.doe@gmail.com",
+				Id:    2,
+			},
+		},
+	}
+}
+
+type FormData struct {
+	Errors map[string]string
+	Values map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Errors: map[string]string{},
+		Values: map[string]string{},
+	}
+}
+
+type PageData struct {
+	Data Data
+	Form FormData
+}
+
+func NewContact(id int, name, email string) Contact {
+	return Contact{
+		Id:    id,
+		Name:  name,
+		Email: email,
+	}
+}
+
+func NewPageData(data Data, form FormData) PageData {
+	return PageData{
+		Data: data,
+		Form: form,
+	}
+}
+
+func contactExists(contacts []Contact, email string) bool {
+	for _, c := range contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 
-    e := echo.New()
+	e := echo.New()
 
-    count := Count{Count: 0}
+	data := NewData()
+	id := 3
 
-    e.Renderer = newTemplate()
+	e.Renderer = newTemplate()
+	e.Use(middleware.Logger())
 
-    e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index.html", count)
-    });
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index.html", NewPageData(*data, NewFormData()))
+	})
 
-    e.POST("/count", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "count", count)
-    });
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
 
-    e.Logger.Fatal(e.Start(":42069"))
+		if contactExists(data.Contacts, email) {
+			formData := FormData{
+				Errors: map[string]string{
+					"email": "Email already exists",
+				},
+				Values: map[string]string{
+					"name":  name,
+					"email": email,
+				},
+			}
+
+			return c.Render(422, "contact-form", formData)
+		}
+
+		contact := NewContact(id, name, email)
+		id++
+		data.Contacts = append(data.Contacts, contact)
+
+		formData := NewFormData()
+		err := c.Render(200, "contact-form", formData)
+
+		if err != nil {
+			return err
+		}
+
+		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+        id, err := strconv.Atoi(idStr)
+
+        if err != nil {
+            return c.String(400, "Id must be an integer")
+        }
+
+        deleted := false
+		for i, contact := range data.Contacts {
+			if contact.Id == id {
+				data.Contacts = append(data.Contacts[:i], data.Contacts[i+1:]...)
+                deleted = true
+                break
+			}
+		}
+
+        if !deleted {
+            return c.String(400, "Contact not found")
+        }
+
+		return c.NoContent(200)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
 }
-
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.0
 require github.com/labstack/echo/v4 v4.11.2
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -14,4 +15,5 @@ require (
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
