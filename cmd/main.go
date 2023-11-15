d27daae5783800b02ae96023897d015f183f3839
mpaulson <the.primeagen@gmail.com>
2023-11-15 12:20:42 -0700

    the ending was great.

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,15 +24,19 @@ func newTemplate() *Templates {
     }
 }
 
+var id = 0
 type Contact struct {
     Name string
     Email string
+    Id int
 }
 
 func newContact(name, email string) Contact {
+    id++
     return Contact{
         Name: name,
         Email: email,
+        Id: id,
     }
 }
 
@@ -38,6 +44,15 @@ type Contacts = []Contact
 
 type Data struct {
     Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+    for i, contact := range d.Contacts {
+        if contact.Id == id {
+            return i
+        }
+    }
+    return -1
 }
 
 func (d *Data) hasEmail(email string) bool {
@@ -90,6 +105,9 @@ func main() {
     page := newPage()
     e.Renderer = newTemplate()
 
+    e.Static("/images", "images")
+    e.Static("/css", "css")
+
     e.GET("/", func(c echo.Context) error {
         return c.Render(200, "index", page)
     })
@@ -113,6 +131,25 @@ func main() {
         // TODO: ??????
         c.Render(200, "form", newFormData())
         return c.Render(200, "oob-contact", contact)
+    })
+
+    e.DELETE("/contacts/:id", func(c echo.Context) error {
+        time.Sleep(3 * time.Second)
+
+        idStr := c.Param("id")
+        id, err := strconv.Atoi(idStr)
+        if err != nil {
+            return c.String(400, "Invalid id")
+        }
+
+        index := page.Data.indexOf(id)
+        if index == -1 {
+            return c.String(404, "Contact not found")
+        }
+
+        page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+        return c.NoContent(200)
     })
 
 
